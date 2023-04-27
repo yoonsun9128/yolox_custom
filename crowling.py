1143cc1de078d20e0ab5e0868cfcfb487b06f93b
yoonsun <bristol9128@naver.com>
2023-04-27 18:01:56 +0900

    crowling

--- crowling.py
+++ crowling.py
@@ -10,33 +10,44 @@ import os
 
 
 driver = webdriver.Chrome('/Users/triplet/Downloads/chromedriver_mac64') # 크롬드라이버 설치한 경로 작성 필요 
-url = "https://auto.danawa.com/auto/?Work=home"
+url = "https://www.google.co.kr/search?q=car&hl=ko&authuser=0&tbm=isch&source=hp&biw=1440&bih=821&ei=ojZKZJGqCsan-QbSrb2wAQ&iflsig=AOEireoAAAAAZEpEspPgoUXtfwUu2scN7yOsYF8Eggyp&ved=0ahUKEwiRg5DB1sn-AhXGU94KHdJWDxYQ4dUDCAc&uact=5&oq=car&gs_lcp=CgNpbWcQAzIICAAQgAQQsQMyCAgAEIAEELEDMggIABCABBCxAzIICAAQgAQQsQMyCAgAEIAEELEDMgUIABCABDIICAAQgAQQsQMyBQgAEIAEMgUIABCABDIICAAQgAQQsQM6BAgAEANQAFiQBGDyCGgAcAB4AIABcYgBvAKSAQMxLjKYAQCgAQGqAQtnd3Mtd2l6LWltZw&sclient=img"
 driver.get(url)
 
-urls = driver.find_elements(By.CLASS_NAME,'branditem')
-url_list = []
+name = 'car'
+path = './img'
 
-img_folder = './img'
+imgs = driver.find_elements(By.CSS_SELECTOR,".rg_i.Q4LuWd")
 
-file_list = os.listdir(img_folder)
-file_count = len(file_list)
-print(file_count)
+for img in imgs:
+    try:
+        time.sleep(2)
+        imgUrl = img.get_attribute("src")
+        print(imgUrl)
+        urllib.request.urlretrieve(imgUrl, path + name + str(count) + ".jpg")
+        count = count + 1
+        if count > 100: #다운 받을 이미지 갯수 조정
+            break
+    except:
+            pass
+# file_list = os.listdir(img_folder)
+# file_count = len(file_list)
+# print(file_count)
 
-for url in urls:
-    time.sleep(1)
-    path = url.find_element(By.TAG_NAME, 'a').get_attribute('href')
-    # print(path)
-    # url_list.append(path)
-    driver.get(path)
-    plants = driver.find_elements(By.TAG_NAME, "img")
-    for plant in plants:
-        car = plant.get_attribute('src')
-        if "model" in car:
-            if file_count< 100:
-                cv2.imwrite(img_folder, car)
-                print(car)
-            else:
-                driver.close()
+# for url in urls:
+#     time.sleep(1)
+#     path = url.find_element(By.TAG_NAME, 'a').get_attribute('href')
+#     # print(path)
+#     # url_list.append(path)
+#     driver.get(path)
+#     plants = driver.find_elements(By.TAG_NAME, "img")
+#     for plant in plants:
+#         car = plant.get_attribute('src')
+#         if "model" in car:
+#             if file_count< 100:
+#                 cv2.imwrite(img_folder, car)
+#                 print(car)
+#             else:
+#                 driver.close()
 
 
 # for i in url_list:
